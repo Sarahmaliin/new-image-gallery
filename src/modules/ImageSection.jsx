be0b627400eac460f69428e673fbd1a4884f18cd
Sarahmalin <sarahmalinkarlsson@gmail.com>
2022-04-02 19:04:09 +0200

    jobbat mer på fetchning av bilder

--- src/modules/ImageSection.jsx
+++ src/modules/ImageSection.jsx
@@ -5,23 +5,51 @@ const ImageSection =  (url) =>{
 
     const [ data, setData ] = useState()
     const [ images, setImages ] = useState()
+    let imageArr = []
+    const [ newArr, setNewArr ] = useState()
 
 
      axios.get(url.urlData)
      .then(function (response) {
      setData(response.data)
-     setImages(data.photos.photo)
+     setImages(response.data.photos.photo)
+     getData()
      })
 
 
-     
+     const getData = () =>{
+        images ? images.map((image) =>{
 
+            return getFlickrImageUrl(image, 'q')
 
-    
+             }) 
+             : 
+             console.log('not yet')}
+      
+
+             const getFlickrImageUrl = (image, size) =>{
+                let serverId = image.server
+                let id = image.id 
+                let secret = image.secret
+                let imgSrc = `https://live.staticflickr.com/${serverId}/${id}_${secret}_z.jpg` 
+                if (size) {
+                    // Configure image size
+                    imgSrc += `_${size}`;
+                  }
+                  imgSrc += '.jpg';
+                  setNewArr({imgSrc})
+                  return imgSrc;
+                };
+
     return(
         <section>
             <h1>Images goes here</h1>
-            
+            {imageArr ?  imageArr.map((image, i) =>{
+                <figure key = {i}>
+                    <img src={image} alt="" />
+                </figure>
+                
+            }) : null}
         </section>
     )
 }
